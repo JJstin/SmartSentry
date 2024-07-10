f5bd5d4f60e8cf617744ae4f7f23cf5f712519cc
George Jiang <george.jj.jiang@gmail.com>
2024-07-10 16:39:14 -0400

    Remove deleted files

---
+++ camera_fw/vitis/camera_platform/ps7_cortexa9_0/standalone_ps7_cortexa9_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,11 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\Users\\georg\\Documents\\UWaterloo\\4A\\FYDP\\camera_repo\\SmartSentry\\camera_fw\\vitis\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\sleep.h"
+  "C:\\Users\\georg\\Documents\\UWaterloo\\4A\\FYDP\\camera_repo\\SmartSentry\\camera_fw\\vitis\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xiltimer.h"
+  "C:\\Users\\georg\\Documents\\UWaterloo\\4A\\FYDP\\camera_repo\\SmartSentry\\camera_fw\\vitis\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xtimer_config.h"
+  "C:\\Users\\georg\\Documents\\UWaterloo\\4A\\FYDP\\camera_repo\\SmartSentry\\camera_fw\\vitis\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\lib\\libxiltimer.a"
+  )
+endif()
---
+++ camera_fw/vivado/camera_hw/camera_hw.gen/sources_1/bd/camera_design/ip/camera_design_proc_sys_reset_0_0/camera_design_proc_sys_reset_0_0_stub.vhdl
@@ -0,0 +1,40 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+-- Date        : Wed Jul 10 16:23:31 2024
+-- Host        : GJiang-Laptop running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/georg/Documents/UWaterloo/4A/FYDP/camera_repo/SmartSentry/camera_fw/vivado/camera_hw/camera_hw.gen/sources_1/bd/camera_design/ip/camera_design_proc_sys_reset_0_0/camera_design_proc_sys_reset_0_0_stub.vhdl
+-- Design      : camera_design_proc_sys_reset_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity camera_design_proc_sys_reset_0_0 is
+  Port ( 
+    slowest_sync_clk : in STD_LOGIC;
+    ext_reset_in : in STD_LOGIC;
+    aux_reset_in : in STD_LOGIC;
+    mb_debug_sys_rst : in STD_LOGIC;
+    dcm_locked : in STD_LOGIC;
+    mb_reset : out STD_LOGIC;
+    bus_struct_reset : out STD_LOGIC_VECTOR ( 0 to 0 );
+    peripheral_reset : out STD_LOGIC_VECTOR ( 0 to 0 );
+    interconnect_aresetn : out STD_LOGIC_VECTOR ( 0 to 0 );
+    peripheral_aresetn : out STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end camera_design_proc_sys_reset_0_0;
+
+architecture stub of camera_design_proc_sys_reset_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "slowest_sync_clk,ext_reset_in,aux_reset_in,mb_debug_sys_rst,dcm_locked,mb_reset,bus_struct_reset[0:0],peripheral_reset[0:0],interconnect_aresetn[0:0],peripheral_aresetn[0:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "proc_sys_reset,Vivado 2024.1";
+begin
+end;
